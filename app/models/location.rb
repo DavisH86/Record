d376baf83dbebbcf282548a864980898db3568f8
Davis Ma <davishma@gmail.com>
2019-01-03 19:27:32 -0500

    add location controller

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
   validates :name, presence: true
   validates :address, pressence: true
+  validates :country, presence: true
 
-  has_many :events 
+  has_many :events
 end
